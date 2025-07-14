c9f5149c270aee11aa70b4eeb150c195918e6ac0
pocketfood <johnfrayster@gmail.com>
2025-07-14 15:26:58 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import WatchPage from './pages/WatchPage';
+import WatchMultiPage from './pages/WatchMultiPage'; // ← NEW import
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/v/:id" element={<WatchPage />} />
+      <Route path="/m/:id" element={<WatchMultiPage />} /> {/* ← NEW route */}
     </Routes>
   );
 }
