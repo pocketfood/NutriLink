1b7dd41ac6090fd4d1546aa104d7126301697476
pocketfood <crazykozy@protonmail.ch>
2026-01-12 19:29:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import WatchPage from './pages/WatchPage';
+import WatchEntryPage from './pages/WatchEntryPage';
 import MultiTrackPage from './pages/MultiTrackPage';
-import WatchMultiPage from './pages/WatchMultiPage'; // ← NEW import
+import WatchMultiPage from './pages/WatchMultiPage';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/v/:id" element={<WatchPage />} />
+      <Route path="/v/*" element={<WatchEntryPage />} />
       <Route path="/studio" element={<MultiTrackPage />} />
       <Route path="/studio/:id" element={<MultiTrackPage />} />
-      <Route path="/m/:id" element={<WatchMultiPage />} /> {/* ← NEW route */}
+      <Route path="/m/:id" element={<WatchMultiPage />} />
     </Routes>
   );
 }
